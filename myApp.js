ad51163e6bd5a93d694475a1217238217b6c9d0c
Hakai <moinkhanrobin1@gmail.com>
2023-12-27 01:54:08 +0530

    Get query param Input From Client --FIX 9

--- myApp.js
+++ myApp.js
@@ -45,7 +45,9 @@ app.get('/:word/echo',((req,res)=>{
 
 app.get('/name',((req,res)=>{
     let {first:firstName,last:lastName} = req.query;
-    res.json(`{name:${firstName} ${lastName}}`)
+    res.json({
+        name:`${firstName} ${lastName}`
+    })
 }))
 app.get('/json',(req,res)=>{
     const jsonMsg ={
