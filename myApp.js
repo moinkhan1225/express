5e446c469085a1b82cc4cc99b0904d36a2fb9785
Hakai <moinkhanrobin1@gmail.com>
2023-12-26 16:28:34 +0530

    Adding Css using USE method

--- myApp.js
+++ myApp.js
@@ -3,12 +3,13 @@ let app = express();
 console.log("Hello World");
 
 const path = __dirname + '/views/index.html';
+const absolutePath = __dirname + "/public";
 
 app.get('/',(req,res)=>{
 res.sendFile(path);
 })
 
-
+app.use(absolutePath,express.static())
 
 
 
