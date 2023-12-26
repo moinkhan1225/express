065d35e3e96d089132d5431131078d6b41d2e526
Hakai <moinkhanrobin1@gmail.com>
2023-12-26 16:22:22 +0530

    Send HTML file

--- myApp.js
+++ myApp.js
@@ -2,8 +2,10 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+const path = __dirname + '/views/index.html';
+
 app.get('/',(req,res)=>{
-res.send("Hello Express")
+res.sendFile(path);
 })
 
 
